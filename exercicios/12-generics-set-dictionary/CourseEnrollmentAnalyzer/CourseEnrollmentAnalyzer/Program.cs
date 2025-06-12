b0634c277675053449dc496aaf321b2c42792fd8
G-OliverDev <180020076+G-OliverDev@users.noreply.github.com>
2025-06-12 10:50:33 -0300

    exercícios propostos da seção generics, set, dictionary

---
+++ exercicios/12-generics-set-dictionary/CourseEnrollmentAnalyzer/CourseEnrollmentAnalyzer/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace CourseEnrollmentAnalyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> courseA = [];
+            HashSet<int> courseB = [];
+            HashSet<int> courseC = [];
+
+            Console.Write("How many students for course A? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int code = int.Parse(Console.ReadLine());
+                courseA.Add(code);
+            }
+
+            Console.Write("How many students for course B? ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int code = int.Parse(Console.ReadLine());
+                courseB.Add(code);
+            }
+
+            Console.Write("How many students for course C? ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int code = int.Parse(Console.ReadLine());
+                courseC.Add(code);
+            }
+
+            HashSet<int> students = new HashSet<int>(courseA);
+            students.UnionWith(courseB);
+            students.UnionWith(courseC);
+
+            Console.WriteLine("Total students: " + students.Count);
+        }
+    }
+}
---
+++ exercicios/12-generics-set-dictionary/ElectionVoteCounter/ElectionVoteCounter/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ElectionVoteCounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> result = new Dictionary<string, int>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] data = sr.ReadLine().Split(',');
+                        string name = data[0];
+                        int votes = int.Parse(data[1]);
+
+                        if (result.ContainsKey(name))
+                        {
+                            result[name] += votes;
+                        }
+                        else
+                        {
+                            result[name] = votes;
+                        }
+                    }
+
+                    foreach (var candidate in result)
+                    {
+                        Console.WriteLine($"{candidate.Key} : {candidate.Value}");
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
